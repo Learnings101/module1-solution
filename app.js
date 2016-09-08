b7cd28f229ac06d69f6b45d75565c7b423c11e99
Dan Sajjad <dan.sajjad@gmail.com>
2016-09-08 14:56:20 -0400

    Build out the controller and attached function to button using ng-click

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 (function () {
+  'use strict'; 
+
   angular.module("LunchCheck", [])
+
   .controller('LunchFormController', function($scope) {
     $scope.lunches = "";
-    $scope.message = "This is empty";
+    $scope.message = "";
+
+    $scope.displayMessage = function () {
+      var count = countLunches($scope.lunches);
+      if(!count) $scope.message = "Please enter data first";
+      if(count && count <= 3) $scope.message = "Enjoy!";
+      else if(count > 3)  $scope.message = "Too much!";
+    };
+
+    var countLunches = function (str) {
+      if(str.length === 0) return 0;
+      var lunchArray = str.split(',');
+      return lunchArray.length;
+    };
+
   });
+
 })(); 
